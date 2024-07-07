292b75c230bacc0dc3fb4c9fc41677bd43d00c9b
muzu-85 <muzukondeth85@gmail.com>
2024-07-07 18:00:52 +0530

    fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(const MaterialApp(home: MyApp()));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  runApp(const MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -16,8 +24,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: null,
-        body: WillPopScope(
+      body: SafeArea(
+        child: WillPopScope(
           onWillPop: () async {
             if (await _webViewController.canGoBack()) {
               _webViewController.goBack();
@@ -26,15 +34,15 @@ class _MyAppState extends State<MyApp> {
               return true;
             }
           },
-          child: SafeArea(
-            child: WebView(
-              initialUrl: "https://www.amazon.com/",
-              javascriptMode: JavascriptMode.unrestricted,
-              onWebViewCreated: (controller) {
-                _webViewController = controller;
-              },
-            ),
+          child: WebView(
+            initialUrl: "https://www.amazon.com/",
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (controller) {
+              _webViewController = controller;
+            },
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
